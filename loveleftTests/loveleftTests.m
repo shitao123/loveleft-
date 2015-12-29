e801f189d05f169241d36e5f231ae495274ed449
qianfeng <qianfeng@shitao.local>
2015-12-29 09:31:44 +0800

    Initial Commit

---
+++ loveleftTests/loveleftTests.m
@@ -0,0 +1,39 @@
+//
+//  loveleftTests.m
+//  loveleftTests
+//
+//  Created by qianfeng on 15/12/29.
+//  Copyright © 2015年 qianfeng. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface loveleftTests : XCTestCase
+
+@end
+
+@implementation loveleftTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
